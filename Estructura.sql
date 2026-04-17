54af7a80688032ab550a68f8712114b2d408a1f7
PabloGreus <pablogreusvazquez@gmail.com>
2026-04-17 09:09:17 +0200

    primer paquete

---
+++ Estructura.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE LoveCode;
+USE LoveCode;
+-----------------------------------------------------------------------------------
+CREATE TABLE IF NOT exists usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    correo VARCHAR(255) NOT NULL UNIQUE,
+    bio text,
+    fecha_registro datetime not null default now()
+);
+-----------------------------------------------------------------------------------
+CREATE TABLE Tecnologia (
+    id_tecnologia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre  VARCHAR(50)  NOT NULL UNIQUE,
+);
+-----------------------------------------------------------------------------------
+CREATE TABLE Usuarios_Tecnologias (
+    id_usuario INT  NOT NULL,
+    id_tecnologia INT NOT NULL,
+    PRIMARY KEY (id_usuario, id_tecnologia),
+
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario) 
+    REFERENCES usuario(id_usuario) 
+    ON DELETE CASCADE,
+        
+    CONSTRAINT fk_tecnologia FOREIGN KEY (id_tecnologia) 
+    REFERENCES Tecnologia(id_tecnologia) 
+    ON DELETE CASCADE
+);
+-----------------------------------------------------------------------------------
+CREATE TABLE Likes (
+    id_like INT NOT NULL AUTO_INCREMENT,
+    id_emisor INT NOT NULL,
+    id_receptor INT NOT NULL,
+    fecha DATETIME NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (id_like),
+    UNIQUE (id_emisor, id_receptor),
+
+    CONSTRAINT fk_emisor FOREIGN KEY (id_emisor)   
+    REFERENCES usuario(id_usuario) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_receptor FOREIGN KEY (id_receptor) 
+    REFERENCES usuario(id_usuario) ON DELETE CASCADE
+);
+-----------------------------------------------------------------------------------
+CREATE TABLE Matches (
+    id_matches INT NOT NULL AUTO_INCREMENT,
+    id_usuario1 INT NOT NULL,   
+    id_usuario2 INT NOT NULL,   
+    fecha DATETIME NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (id),
+    UNIQUE (id_usuario1, id_usuario2),
+
+    CONSTRAINT fk_match_u1 FOREIGN KEY (id_usuario1) 
+        REFERENCES usuario(id_usuario) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_match_u2 FOREIGN KEY (id_usuario2) 
+        REFERENCES usuario(id_usuario) ON DELETE CASCADE
+);
